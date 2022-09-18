fdc7a2207c66fd97ddf7497b9efdef463edd64f5
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-18 15:56:22 +0300

    Footer component: UI design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,140 @@
+import {
+  EmailOutlined,
+  Facebook,
+  Instagram,
+  LocationOnOutlined,
+  PhoneIphoneOutlined,
+  Pinterest,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+export default function Footer() {
+  return (
+    <Container>
+      <Left>
+        <Logo>SHOPPING.io</Logo>
+        <Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+          quibusdam debitis nobis, earum aliquid ipsam ut eum, nihil suscipit
+          facilis inventore? Molestias similique voluptatibus mollitia
+          accusantium officia quo ut facere.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="e4405f">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55acee">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="e60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men Fashion</ListItem>
+          <ListItem>Women Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms and Conditions</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <LocationOnOutlined style={{ marginRight: "10px" }} />
+          AdminVille, 11235 Lower Heights, Nairobi
+        </ContactItem>
+        <ContactItem>
+          <PhoneIphoneOutlined style={{ marginRight: "10px" }} />
+          +254 701234567
+        </ContactItem>
+        <ContactItem>
+          <EmailOutlined style={{ marginRight: "10px" }} />
+          kleptomaniac@shopping.io
+        </ContactItem>
+        <Payment src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/M-PESA_LOGO-01.svg/1200px-M-PESA_LOGO-01.svg.png" />
+      </Right>
+    </Container>
+  );
+}
