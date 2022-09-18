2d97184a2723af805d9aa2fa40c10551e1a5b8f2
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-18 03:01:45 +0300

    data.js: dummy data for slider

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/2049411/pexels-photo-2049411.jpeg?cs=srgb&dl=pexels-cristian-benavides-2049411.jpg&fm=jpg",
+    title: "Summer Sale!",
+    desc: "Don't compromise on style. Get 30% Off for New Arrivals",
+    bg: "d5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1661471/pexels-photo-1661471.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Amazing Summer Sale!",
+    desc: "Don't compromise on style. Get 40% Off This",
+    bg: "f54add",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Easter Sale!",
+    desc: "Gadgets Galore. Get 30% Off for Latest Arrivals",
+    bg: "f3cafd",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Black Friday!",
+    desc: "Get 30% Off for the New iPhone!",
+    bg: "e3fcfd",
+  },
+];
