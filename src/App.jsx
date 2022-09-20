580feb7a29d9e23ab02c471be310bed6e99ccf33
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-20 20:01:24 +0300

    Register and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 };
---
+++ src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url("https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+      center;
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 20%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 14px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+export default function Login() {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Sign In</Title>
+        <Form>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Button>Sign In</Button>
+          <Link>Forgot Password?</Link>
+          <Link>Create New Account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+}
